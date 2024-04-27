f012a1c2e26c403d21d0552721ed5662fa2ecd1b
DeeBest <simphiwedladla8@gmail.com>
2024-04-27 13:41:08 +0200

    added responsiveness

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
 const footerParagraph = document.getElementById('footer-paragraph');
 const currentDate = new Date().getFullYear();
 footerParagraph.textContent = `Created by \xA9Simphiwe ${currentDate}.`;
+
+updateTime();
+
+function updateTime() {
+  let hoursText = document.getElementById('hours');
+  hoursText.textContent = new Date().getHours();
+  hoursText.textContent < 10
+    ? (hoursText.textContent = '0' + hoursText.textContent)
+    : (hoursText.textContent = hoursText.textContent);
+
+  let minutesText = document.getElementById('minutes');
+  minutesText.textContent = new Date().getMinutes();
+  minutesText.textContent < 10
+    ? (minutesText.textContent = '0' + minutesText.textContent)
+    : (minutesText.textContent = minutesText.textContent);
+
+  let secondsText = document.getElementById('seconds');
+  secondsText.textContent = new Date().getSeconds();
+  secondsText.textContent < 10
+    ? (secondsText.textContent = '0' + secondsText.textContent)
+    : (secondsText.textContent = secondsText.textContent);
+
+  let amPm = document.getElementById('am-pm');
+
+  if (hoursText.textContent >= 12) {
+    amPm.textContent = 'PM';
+    amPm.style.backgroundColor = 'black';
+    amPm.style.color = 'white';
+    amPm.style.opacity = '0.7';
+  } else {
+    amPm.textContent = 'AM';
+    amPm.style.backgroundColor = 'white';
+    amPm.style.color = 'black';
+    amPm.style.opacity = '0.7';
+  }
+  setTimeout(() => {
+    updateTime();
+  }, 1000);
+}
